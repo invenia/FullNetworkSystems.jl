0080a6b59d948f84ace8d918206fbbdd1ef5637d
Branwen Snelling <branwen.snelling@invenialabs.co.uk>
2022-04-28 10:40:28 +0100

    -DEV version

--- src/system.jl
+++ src/system.jl
@@ -108,7 +108,7 @@ end
 The abstract type for representing the whole power system including topology, static
 components and their attributes, and time series data.
 
-Topology: `Dictionary`s linking generators, loads, and bids (if present) to buses.
+Topology: `Dictionaries` linking generators, loads, and bids (if present) to buses.
 System wide static components and grid matrices: zones, buses, generators, branches, LODF and PTDF.
 Time series data: all the time series associated with generators, loads and bids.  All stored
 as `KeyedArray`s of `ids x datetimes`.
